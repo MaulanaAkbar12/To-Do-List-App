2e11e6fa3b95368179702ba6cb6c6c0ed6e40166
MaulanaAkbar12 <udinu2880@gmail.com>
2024-12-23 13:51:34 +0700

    Update TaskContract Contract.java
    jenkins Change

--- app/src/main/java/com/example/to_dolistapp/TaskContract.java
+++ app/src/main/java/com/example/to_dolistapp/TaskContract.java
@@ -5,18 +5,20 @@ import android.provider.BaseColumns;
 public class TaskContract {
     private TaskContract() {}
 
+    // Table for Category
     public static final class CategoryEntry implements BaseColumns {
         public static final String TABLE_NAME = "categories";
         public static final String COLUMN_ID = "id";
         public static final String COLUMN_NAME = "name";
     }
-
+    // Table for Priority
     public static final class PriorityEntry implements BaseColumns {
         public static final String TABLE_NAME = "priorities";
         public static final String COLUMN_ID = "id";
         public static final String COLUMN_NAME = "name";
     }
 
+    // Table for Task
     public static final class TaskEntry implements BaseColumns {
         public static final String TABLE_NAME = "tasks";
         public static final String COLUMN_TASK = "task";
